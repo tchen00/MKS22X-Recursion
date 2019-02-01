1cac753fce2ab2598462317a818279764e2c1816
Tammy <tchen00@stuy.edu>
2019-01-31 22:02:35 -0500

    finished typing up sqrt from yesterdays hw

--- Recursion.java
+++ Recursion.java
@@ -7,19 +7,30 @@ public class Recursion{
 
     */
     public static double sqrt(double n, double tolerance){
-
+      return sqrtHelper(n, 1, tolerance);
     }
 
+    private static double sqrtHelper(double n, double g, double tolerance) {
+      if (n == 0){
+        return 0;
+      } else if (((g*g - n ) / n ) <= tolerance) {
+        return g;
+      }
+      return sqrtHelper(n, ((n/g +g)/2), tolerance);
+    }
     /*Recursively find the n'th fibbonaci number in linear time
      *fib(0) = 1; fib(1) = 1; fib(5) = 5
      *precondition: n is non-negative
      */
+    /*
     public static int fib(int n){
 
     }
-
+    */
     /*As Per classwork*/
+  /*
     public static ArrayList<Integer> makeAllSums(){
-    }
 
+    }
+  */
 }
